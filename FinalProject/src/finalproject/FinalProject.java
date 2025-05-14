9e1b9fa602693cbfc9d677a044aa33de7f6adaae
3013036 <3013036@10.50.12.146>
2025-05-14 09:46:11 -0400

    Add helper classes

--- FinalProject/src/finalproject/FinalProject.java
+++ FinalProject/src/finalproject/FinalProject.java
@@ -4,6 +4,7 @@ public class FinalProject
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
-    }  
+        Application app = new Application("APCSA Final", 1280, 720);
+        app.run();
+    }
 }
